2fa9a7082b28f756636864d90f75cca7188c4f34
AlanDAmario <vega93@live.it>
2024-06-20 21:31:56 +0200

    [upd] add createApp and api call

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+const { createApp } = Vue;
 
+createApp({
+  data() {
+    return {
+      artistUrl: "http://localhost/php-dischi-json/server.php",
+      artAutor: [],
+    };
+  },
+  methods: {
+    //chiamata api server
+    getArtist() {
+      axios.get(this.artistUrl).then((response) => {
+        this.artAutor = response.data;
+      });
+    },
+  },
+  created() {
+    this.getArtist();
+  },
+}).mount("#app");
